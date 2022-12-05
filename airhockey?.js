d859d1273f6564333f3d269dea4ca9a6451829dd
jaed-npc <111005661+jaed-npc@users.noreply.github.com>
2022-12-04 20:45:52 -0500

    Update airhockey?.js

--- airhockey?.js
+++ airhockey?.js
@@ -208,3 +208,250 @@ var draw = function() {
 **************************/
 
 
+
+
+
+ 
+var m=createFont("monospace");
+var player1Y = height/2;
+var player2Y = height/2;
+var player1Score = 0;
+var player2Score = 0;
+var ball;
+var gameStarted = false;
+var t = 0;
+
+//Constants
+var PAUSE_TIME = 60;
+var PLAYER_MOVE_SPEED = 2;
+var BALL_SPEED = 6.4;
+var PADDLE_HEIGHT = 49;
+var PADDLE_WIDTH = 47;
+
+angleMode = "degrees";
+
+var Ball = function(position, speed) {
+    this.position = position;
+    this.speed = speed || BALL_SPEED;
+    
+    this.radius = 12;
+    
+    this.resetVelocity = function() {
+        this.theta = random(0, 360);
+        this.velocity = new PVector(
+        this.speed*cos(this.theta), -this.speed*sin(this.theta));
+        player2Y = height/2;
+    };
+    this.resetVelocity();
+    
+    this.draw = function() {
+    noStroke();
+    fill(59, 38, 59);
+    ellipse(this.position.x,this.position.y,22,22);
+    fill(28, 6, 28);
+    ellipse(this.position.x,this.position.y,16,16);
+    };
+    
+    this.collideWithPaddle = function(x, y) {
+        if (this.position.x - this.radius < x + PADDLE_WIDTH/2 &&
+        this.position.x + this.radius > x - PADDLE_WIDTH/2) {
+            if (dist(0, this.position.y, 0, y) <
+            PADDLE_HEIGHT/2 + this.radius) {
+                if (this.position.x > x) {
+                    this.position.x = x +
+                    this.radius + PADDLE_WIDTH/2;
+                }
+                else if (this.position.x < x) {
+                    this.position.x = x - 
+                    this.radius - PADDLE_WIDTH/2;
+                }
+                this.velocity.mult(new PVector(-1, 1));
+            }
+        }
+    };
+    
+    this.update = function() {
+        //Handle wall collisions
+        if (this.position.x < 10 && this.position.y>130 && this.position.y<280) {
+            player2Score++;
+            this.position = new PVector(width/2, height/2);
+            gameStarted = false;
+            this.resetVelocity();
+        }
+        else if (this.position.x > width && this.position.y>140 && this.position.y<280) {
+            player1Score++;
+            this.position = new PVector(width/2, height/2);
+            gameStarted = false;
+            this.resetVelocity();
+        }
+       
+       else if (this.position.y < 0) {
+            this.position.y = 0;
+            this.velocity.mult(new PVector(1, -1));
+        }
+        else if (this.position.y > height) {
+            this.position.y = height;
+            this.velocity.mult(new PVector(1, -1));
+        }
+        else if (this.position.x>width){
+            this.position.x=390;
+            this.velocity.mult(new PVector(-1, 1));
+        }
+        else if(this.position.x<5){
+            this.position.x=10;
+            this.velocity.mult(new PVector(-1, 1));
+        }
+        
+        
+        //Handle paddle collisions
+        this.collideWithPaddle(20, player1Y);
+        this.collideWithPaddle(width-20, player2Y);
+        
+        this.position.add(this.velocity);
+    };
+};
+
+ball = new Ball(new PVector(width/2, height/2));
+
+var drawScores = function() {
+    fill(3, 0, 0);
+    stroke(117, 117, 117);
+    rect(99,6,208,24,39);
+    rect(202,12,0,20);
+    var s;
+    textFont(m);
+    fill(255, 0, 0);
+    
+    textSize(15);
+    //textFont(m);
+    
+    s = "Player 1:"+player1Score;
+    text(s, width*0.37-textWidth(s)/2, 25);
+    s = "Player 2:"+player2Score;
+    text(s, width*0.64-textWidth(s)/2, 25);
+};
+
+var updatePlayer2 = function() {
+    if (abs(player2Y-ball.position.y) < PLAYER_MOVE_SPEED){
+        player2Y = ball.position.y;
+    }
+    else if (player2Y-ball.position.y >= PLAYER_MOVE_SPEED) {
+        player2Y -= PLAYER_MOVE_SPEED;
+    }
+    else if (player2Y-ball.position.y <= PLAYER_MOVE_SPEED) {
+        player2Y += PLAYER_MOVE_SPEED;
+    }
+    
+};
+
+//Move the player up
+var movePlayerUp = function() {
+    player1Y -= PLAYER_MOVE_SPEED;
+};
+
+//Move the player down
+var movePlayerDown = function() {
+    player1Y += PLAYER_MOVE_SPEED;
+};
+
+var drawPlayers = function() {
+    //Constrain the player movement
+   player1Y = constrain(player1Y, 0, 400);
+    
+    fill(204, 0, 0);
+    strokeWeight(2.5);
+    ellipse(18,player1Y , PADDLE_WIDTH, PADDLE_HEIGHT);
+    stroke(255, 0, 0);
+    strokeWeight(20);
+    point(18,player1Y);
+    stroke(135, 23, 23);
+    strokeWeight(10);
+    point(18,player1Y);
+    
+    fill(37, 0, 245);
+    stroke(0, 30, 255);
+    ellipse(width-20, player2Y, PADDLE_WIDTH-5, PADDLE_HEIGHT-9);
+     
+     stroke(25, 86, 135);
+    strokeWeight(20);
+    point(width-20,player2Y);
+    stroke(0, 8, 125);
+    strokeWeight(10);
+    point(width-20,player2Y);
+};
+
+draw = function() {
+    //Control Player 1
+
+    //Draw the environment
+    background(0, 145, 19);
+    stroke(0, 0, 0);
+    strokeWeight(2);
+    fill(255, 255, 255);
+    rect(0, 0, 399, 399, 50);
+    
+    
+     for (var g=0; g<=1; g++)
+        {
+        //Goal
+            noStroke();
+            fill(136, 219, 240);
+            rect(-50+392*g,120, 106, 167, 50);
+        //Blue Lines
+            fill(0, 132, 255);
+            rect(313*g,-9*g,3*g,413*g);
+            rect(88*g,-9*g,3*g,413*g);
+            
+        }
+        
+        //Center Ice
+    noStroke();
+    fill(245, 8, 8);
+    rect(200.5, 9, 3, 405);
+//Circle
+    fill(105, 225, 255);
+    ellipse(201, 199, 50, 50);
+//Net
+    stroke(255, 0, 0);
+    strokeWeight(5);
+    arc(381, 205, 136, 145, -88, 88); //Top
+    arc(17, 205, 136, -145, 93, 271); //Bottom
+    
+     for (var v=1; v < 20; v++)
+                {
+            for (var h=1; h < 20; h++)
+            {
+                strokeWeight(3);
+                stroke(222, 222, 222);
+                point(2+20*h,20*v);
+        }
+    }
+
+    
+    updatePlayer2();
+    drawPlayers();
+    drawScores();
+  
+    
+    //Draw the ball
+    ball.draw();
+    
+    if (!gameStarted) {
+        t++;
+        if (t >= PAUSE_TIME) {
+            t = 0;
+            gameStarted = true;
+        }
+        return;
+    }
+    if (keyIsPressed) {
+        if (keyCode === UP){
+            movePlayerUp();
+        }
+        else if (keyCode === DOWN){
+            movePlayerDown();
+        }
+    }
+    ball.update();
+};
+
